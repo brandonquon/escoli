a3086f9f48465bedd0e6ab30c7df4056ff38feb0
geitben <bqsbarbq@yahoo.com>
2013-04-17 05:49:34 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Escoli::Application.config.secret_token = '4662e7ec0a16c5d838120c45de6491b23b67fb8f4496c08d5f4902e502069ec7b95b95b20960180e255ab38ebd738a1a235fd992605331ec6bf2771da732ee86'
